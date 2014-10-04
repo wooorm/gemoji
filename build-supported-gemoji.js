9bdc962f17c6682ebd38ce3da4173db629892870
wooorm <tituswormer@gmail.com>
2014-10-04 21:18:09 +0200

    Refactor support generation to use markdown-table

--- build-supported-gemoji.js
+++ build-supported-gemoji.js
@@ -1,4 +1,20 @@
-var fs = require('fs');
+'use strict';
+
+/**
+ * Dependencies.
+ */
+
+var fs,
+    table,
+    gemoji;
+
+fs = require('fs');
+table = require('markdown-table');
+gemoji = require('./data/gemoji');
+
+/**
+ * Escape a unicode emoji.
+ */
 
 function escape(value) {
     return value.split('').map(function (character) {
@@ -6,24 +22,58 @@ function escape(value) {
     }).join('');
 }
 
-var data = require('./data/gemoji.json');
+/**
+ * Create an expression from all emoji.
+ */
+
+var expression;
+
+expression = new RegExp(
+    Object.keys(gemoji).map(function (name) {
+        return gemoji[name];
+    }).join('|'),
+    'g'
+)
+
+/**
+ * Set up data.
+ */
+
+var data;
+
+data = [
+    ['Unicode', 'Name', 'Escaped Unicode']
+];
+
+data = data.concat(
+    Object.keys(gemoji).map(function (name) {
+        return [
+            gemoji[name],
+            name,
+            escape(gemoji[name])
+        ]
+    })
+);
+
+/**
+ * Write support.
+ */
 
 fs.writeFileSync('Supported-Gemoji.md',
     'Supported Gemoji:\n' +
     '=================\n' +
     '\n' +
-    'Note that this file does not contain the gemoji\'s as rendered by' +
-    'GitHub; 870 small images would make viewing this document very slow.\n' +
+    'Note that this file does not contain the gemoji\'s as rendered by ' +
+    'GitHub;\n' + Object.keys(gemoji).length + ' small images would make ' +
+    'viewing this document very slow.\n' +
     '\n' +
-    '| unicode | name | escaped unicode |\n' +
-    '|:-------:|:----:|:---------------:|\n' +
 
-    Object.keys(data).map(function (name) {
-        return '| ' +
-            data[name] + ' | ' +
-            name + ' | ' +
-            escape(data[name]) + ' |'
-    }).join('\n') +
+    table(data, {
+        'align' : ['c', 'c', 'c'],
+        'stringLength' : function (value) {
+            return value.replace(expression, '  ').length;
+        }
+    }) +
 
     '\n'
 );
