98c6c91e8dc67f64cd7c3fa85ba5905945c22f57
Titus Wormer <tituswormer@gmail.com>
2022-11-16 14:50:19 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -2,34 +2,28 @@
  * @typedef {import('./script/build-data.js').Emoji} Emoji
  */
 
-import fs from 'node:fs'
-import assert from 'node:assert'
-import test from 'tape'
+import fs from 'node:fs/promises'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {gemoji} from './index.js'
 
 /** @type {Array<Emoji>} */
-const emoji = JSON.parse(String(fs.readFileSync('emoji.json')))
+const emoji = JSON.parse(String(await fs.readFile('emoji.json')))
 
-test('gemoji', function (t) {
+test('gemoji', function () {
   let index = -1
-
-  t.plan(emoji.length)
 
   while (++index < gemoji.length) {
     const info = gemoji[index]
 
-    t.doesNotThrow(function () {
-      assert.strictEqual(info.emoji, emoji[index].emoji, 'emoji')
-      assert.strictEqual(info.category, emoji[index].category, 'category')
-      assert.strictEqual(
-        info.description,
-        emoji[index].description,
-        'description'
-      )
-      assert.deepStrictEqual(info.names, emoji[index].aliases, 'names')
-      assert.deepStrictEqual(info.tags, emoji[index].tags, 'tags')
-    }, emoji[index].emoji + '   ' + emoji[index].description)
+    assert.strictEqual(info.emoji, emoji[index].emoji, 'emoji')
+    assert.strictEqual(info.category, emoji[index].category, 'category')
+    assert.strictEqual(
+      info.description,
+      emoji[index].description,
+      'description'
+    )
+    assert.deepStrictEqual(info.names, emoji[index].aliases, 'names')
+    assert.deepStrictEqual(info.tags, emoji[index].tags, 'tags')
   }
-
-  t.end()
 })
